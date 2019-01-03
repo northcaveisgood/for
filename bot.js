2de807aa66976ac8ba3196258e1d764efe045a29
northcaveisgood <45044753+northcaveisgood@users.noreply.github.com>
2019-01-03 21:43:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', async() => {
 var server = "509357857666236430"; // ايدي السررفر
 var channel = "509357857666236434";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send(صلاح بحب صلاح الدين وصلاح الدين بحب صحابو وصحابو بحبو وصلاح بحب صحابوا بموت فيهم وههما بحبوا **لحب مهند**)
     },305);
 })
 
